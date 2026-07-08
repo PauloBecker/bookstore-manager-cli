77a5e0625b3ce5fc036814e02915e5bf585a7cf6
Paulo Becker <beckerpaulo81@gmail.com>
2026-07-07 22:19:30 -0300

    "feat: implementa menu principal no main.ts"

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function mainMenu() {
+  console.log("\n=== Bookstore Manager CLI ===");
+  console.log("1 - Gerenciar Autores");
+  console.log("2 - Gerenciar Livros");
+  console.log("3 - Gerenciar Clientes");
+  console.log("4 - Gerenciar Empréstimos");
+  console.log("0 - Sair");
+
+  rl.question("Escolha uma opção: ", (option) => {
+    switch (option) {
+      case "1":
+        console.log("👉 Você escolheu Autores");
+        break;
+      case "2":
+        console.log("👉 Você escolheu Livros");
+        break;
+      case "3":
+        console.log("👉 Você escolheu Clientes");
+        break;
+      case "4":
+        console.log("👉 Você escolheu Empréstimos");
+        break;
+      case "0":
+        console.log("Saindo...");
+        rl.close();
+        //process.exit(0);
+        return;
+      default:
+        console.log("Opção inválida, tente novamente.");
+    }
+    mainMenu(); // reinicia o menu
+  });
+}
+
+mainMenu();

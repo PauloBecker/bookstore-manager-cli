976b4486136a2b243575a33d47aeb2e7d08e2726
Paulo Becker <beckerpaulo81@gmail.com>
2026-07-09 10:36:24 -0300

    Alterações menu

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
-import readline from "readline";
-import pool from "./database";
+import * as readline from "readline";
 
-async function testDB() {
-  try {
-    const res = await pool.query("SELECT NOW()");
-    console.log("📅 Conexão bem-sucedida:", res.rows[0]);
-  } catch (err) {
-    console.error("❌ Erro ao conectar no banco:", err);
-  }
-}
-
-testDB();
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-function mainMenu() {
+
+type MenuOption = "1" | "2" | "3" | "4" | "0";
+
+function mainMenu(): void {
   console.log("\n=== Bookstore Manager CLI ===");
   console.log("1 - Gerenciar Autores");
   console.log("2 - Gerenciar Livros");
@@ -25,30 +17,30 @@ function mainMenu() {
   console.log("4 - Gerenciar Empréstimos");
   console.log("0 - Sair");
 
-  rl.question("Escolha uma opção: ", (option) => {
-    switch (option) {
+  rl.question("Escolha uma opção: ", (option: string) => {
+    switch (option as MenuOption) {
       case "1":
-        console.log("👉 Você escolheu Autores");
+        console.log(" Você escolheu Autores");
         break;
       case "2":
-        console.log("👉 Você escolheu Livros");
+        console.log(" Você escolheu Livros");
         break;
       case "3":
-        console.log("👉 Você escolheu Clientes");
+        console.log(" Você escolheu Clientes");
         break;
       case "4":
-        console.log("👉 Você escolheu Empréstimos");
+        console.log(" Você escolheu Empréstimos");
         break;
       case "0":
         console.log("Saindo...");
         rl.close();
-        //process.exit(0);
         return;
       default:
         console.log("Opção inválida, tente novamente.");
     }
-    mainMenu(); // reinicia o menu
+    mainMenu(); 
   });
 }
+
 
 mainMenu();

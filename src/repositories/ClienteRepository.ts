d4513ba981f819bc56de69e7c7a6a0a179b454b8
Paulo Becker <beckerpaulo81@gmail.com>
2026-07-14 21:33:07 -0300

    feat(clientes) refatorando update e create

--- src/repositories/ClienteRepository.ts
+++ src/repositories/ClienteRepository.ts
@@ -1,10 +1,10 @@
 import pool from "../database/connections/database";
 
 export class ClienteRepository {
-  async create(nome: string) {
+  async create(nome: string, nacionalidade: string, dataNascimento: Date) {
     const result = await pool.query(
-      "INSERT INTO clientes (nome) VALUES ($1) RETURNING *",
-      [nome]
+      "INSERT INTO clientes (nome, nacionalidade, data_nascimento) VALUES ($1, $2, $3) RETURNING *",
+      [nome, nacionalidade, dataNascimento]
     );
     return result.rows[0];
   }
@@ -14,10 +14,10 @@ export class ClienteRepository {
     return result.rows;
   }
 
-  async update(id: number, novoNome: string) {
+  async update(id: number, novoNome: string, novaNacionalidade: string, novaDataNascimento: Date) {
     const result = await pool.query(
-      "UPDATE clientes SET nome = $1 WHERE id = $2 RETURNING *",
-      [novoNome, id]
+      "UPDATE clientes SET nome = $1, nacionalidade = $2, data_nascimento = $3 WHERE id = $4 RETURNING *",
+      [novoNome, novaNacionalidade, novaDataNascimento, id]
     );
     return result.rows[0];
   }
